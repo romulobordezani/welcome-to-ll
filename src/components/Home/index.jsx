b23b8dee6dff401af9373b60c72d1cd2384138c2
romulo bordezani <romulobordezani@gmail.com>
2018-08-26 17:12:02 -0300

    Implements SASS support via css modules in webpack

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -12,7 +12,7 @@ class Home extends Component {
       <div className={styles.App}>
         <header className={styles['App-header']}>
           <img src={logo} className={styles['App-logo']} alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className={styles['App-title']}>Welcome to React</h1>
         </header>
         <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
