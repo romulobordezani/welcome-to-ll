ca3c7a76bf9bc89103b4d20da287d965ec0bbee1
romulo bordezani <romulobordezani@gmail.com>
2018-08-26 17:11:09 -0300

    Implements SASS support via css modules in webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -188,6 +188,53 @@ module.exports = {
               },
             ],
           },
+          // Adding SASS SUpport
+          {
+            test: /\.scss$/,
+            include: [paths.appSrc, paths.appNodeModules],
+            use: [
+              {
+                loader: require.resolve('style-loader'),
+                options: {
+                  sourceMap: true
+                },
+              },
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  modules: true,
+                  localIdentName: "[name]__[local]___[hash:base64:5]"
+                },
+              },
+              require.resolve('resolve-url-loader'),
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+            ],
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './Home.css';
+import styles from './Home.scss';
 
 class Home extends Component {
   constructor(props) {
@@ -9,12 +9,12 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <header className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
