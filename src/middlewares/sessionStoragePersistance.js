63b1aa74a6f5cb9d710f8e96fc6899cb4f208abc
romulo bordezani <romulobordezani@gmail.com>
2018-09-01 12:32:36 -0300

    Adding sessionStoragePersistance via Redux middleware

--- src/middlewares/sessionStoragePersistance.js
+++ src/middlewares/sessionStoragePersistance.js
@@ -1,5 +1,4 @@
 // /* eslint-env browser */
-// eslint-disable-next-line
 const sessionStoragePersistance = store => next => action => {
   if (action.type === 'ADDED_ADDRESS' || action.type === 'REMOVED_ADDRESS') {
     const addressList = JSON.stringify(store.getState().address.list);
